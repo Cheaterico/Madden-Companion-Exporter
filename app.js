f3a66f808feaf10632fe93d136920da36223cf0e
Cheaterico <federico180991@gmail.com>
2017-10-12 11:46:15 +0200

    Update app.js with firebase info

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("./mdl1-mdlseason1-firebase-adminsdk-d34ex-14e6ef1866.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
+  databaseURL: "https://mdl1-mdlseason1.firebaseio.com/"
 });
 
 // Setup
